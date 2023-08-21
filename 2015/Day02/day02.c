0c5a71e2e2d1f1fd7e59708b2ce552662bee9e7d
Misael Abonce <mabonce21@gmail.com>
2023-08-20 22:06:10 -0500

    n/a

--- 2015/Day02/day02.c
+++ 2015/Day02/day02.c
@@ -4,10 +4,24 @@
 int main() {
   int totalSA = 0;
   FILE *inputFile;
+  int currentChar;
+  
+  int l;
+  int w;
+  int h;
 
   inputFile = fopen("input.txt", "r");
 
-  
+  while(1) {
+    currentChar = null; //need to pick up from here on how to read the dimensions
+
+    if(feof(inputFile)) {
+      printf("\nTotal square footage of wrapping paper needed: \n", totalSA);
+      break;
+    }
+
+
+  }
 
   retun 0
 }
